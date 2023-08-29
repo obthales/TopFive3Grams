bbf600a247093e9e5fe5b71db3b0f66460877fff
Thales Vinicius <obthales@gmail.com>
2023-08-29 03:12:16 +0200

    Code refactory.

--- TopFive3Grams/src/main/scala/App.scala
+++ TopFive3Grams/src/main/scala/App.scala
@@ -4,38 +4,56 @@ import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 case class Actor(display_login: String)
+object Actor{
+  implicit val rw: ReadWriter[Actor] = macroRW
+}
+
 case class Commit(message: String)
+object Commit{
+  implicit val rw: ReadWriter[Commit] = macroRW
+}
+
 case class Payload(commits: List[Commit])
+object Payload{
+  implicit val rw: ReadWriter[Payload] = macroRW
+}
+
 case class Item(actor: Actor, payload:Payload)
+object Item{
+  implicit val rw: ReadWriter[Item] = macroRW
+}
 
 object Program {
   def main(args: Array[String]) = {
-    val file = Source.fromFile("10K.github.jsonl")
-    val lines : Iterator[String] = file.getLines
+    val lines : Iterator[String] = ExtractFileContent("10K.github.jsonl")
 
-    implicit val actorRw: ReadWriter[Actor] = macroRW
-    implicit val commitRw: ReadWriter[Commit] = macroRW
-    implicit val payloadRw: ReadWriter[Payload] = macroRW
-    implicit val itemRw: ReadWriter[Item] = macroRW
-
-    val commitsPerAuthor = lines
-      .map( line => try { read[Item](line) } catch { case e => null })
-      .filter(item => item != null && item.payload.commits.nonEmpty)
-      .toSeq
-      .groupBy(_.actor.display_login)
-      .map(item => item._1 -> item._2.flatMap(_.payload.commits).map(_.message))
+    val commitsPerAuthor = CommitsPerAuthor(lines)
 
     val topFiveTrigramsPerAuthor = commitsPerAuthor
-      .map(x => x._1 -> GetTopFiveTrigrams(x._2))
+      .map(x => x._1 -> TopFiveTrigrams(x._2))
 
-    topFiveTrigramsPerAuthor.foreach { x =>
-      val trigrams = x._2.map("'" + _ + "'").mkString(" ")
-      println(s"'${x._1}' $trigrams")
+    topFiveTrigramsPerAuthor.foreach { case (author: String, trigrams: Seq[String]) =>
+      val topFiveTrigrams = trigrams.map("'" + _ + "'").mkString(" ")
+      println(s"'$author' $topFiveTrigrams")
     }
   }
 
-  private def GetTopFiveTrigrams(messages: Seq[String]) = {
-    val trigrams = messages.flatMap(GetMessageTrigrams(_))
+  private def ExtractFileContent(fileName: String) = {
+    val file = Source.fromFile("10K.github.jsonl")
+    file.getLines
+  }
+
+  private def CommitsPerAuthor(lines : Iterator[String]) = lines
+    .map(line => try { read[Item](line) } catch { case ex => null })
+    .filter(item => item != null && item.payload.commits.nonEmpty)
+    .toSeq
+    .groupBy(_.actor.display_login)
+    .map { case (author: String, items: Seq[Item]) =>
+      author -> items.flatMap(_.payload.commits).map(_.message)
+    }
+
+  private def TopFiveTrigrams(messages: Seq[String]) = {
+    val trigrams = messages.flatMap(MessageTrigrams(_))
 
     trigrams
       .groupMapReduce(identity)(_ => 1)(_ + _)
@@ -45,7 +63,7 @@ object Program {
       .take(5)
   }
 
-  private def GetMessageTrigrams(message: String) = {
+  private def MessageTrigrams(message: String) = {
     val words = message.split("\\W+")
 
     val buffer = new ListBuffer[String]()
